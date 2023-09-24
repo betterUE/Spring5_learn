a026b03b9216392bbda811eab29d6871acccc475
betterUE <m17338148416@163.com>
2023-09-24 16:02:21 +0800

    first commit

---
+++ src/test/java/testClassPathContext.java
@@ -0,0 +1,49 @@
+import org.junit.Test;
+import com.test.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class testClassPathContext {
+    @Test
+    public void test1(){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("/applicationContext.xml");
+        //这种方式创建的对象，需要强制转换
+        Person person = (Person) ctx.getBean("person");
+        System.out.println(person);
+    }
+
+    @Test
+    public void test2(){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //这种方法创建的对象，不需要强制转换
+        Person person = ctx.getBean("person", Person.class);
+        System.out.println(person);
+    }
+
+    @Test
+    public void test3(){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //配置文件中定义的bean class,只能有一个是 Person 类型，
+        Person bean = ctx.getBean(Person.class);
+        System.out.println(bean);
+    }
+
+    @Test
+    public void test4(){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //获取所有bean的名称，也就是定义的id
+        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
+        for (int i = 0; i < beanDefinitionNames.length; i++) {
+            System.out.println(beanDefinitionNames[i]);
+        }
+    }
+
+    @Test
+    public void test5(){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        String[] beanNamesForType = ctx.getBeanNamesForType(Person.class);
+        for (int i = 0; i < beanNamesForType.length; i++) {
+            System.out.println(beanNamesForType[i]);
+        }
+    }
+}
